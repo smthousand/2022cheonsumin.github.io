7ebddcac07770915848ffc74b15f6ebd3f108ab1
smthousand <102133017+smthousand@users.noreply.github.com>
2022-04-15 00:58:53 +0900

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -5,8 +5,4 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 });
